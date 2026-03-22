4d5a8dcb93338764b247bf658d8eb08f449c0357
Chris <106016563+Xtosea@users.noreply.github.com>
2026-03-22 04:36:56 +0100

    Update ResetPassword.jsx

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -14,7 +14,7 @@ export default function ResetPassword() {
 
     try {
       const res = await fetch(
-        `http://localhost:5000/api/auth/reset-password/${token}`,
+        `https://afribook-backend.onrender.com/api/auth/reset-password/${token}`,
         {
           method: "POST",
           headers: {
