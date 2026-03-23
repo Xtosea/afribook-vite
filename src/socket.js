533ce5885e570e1ebfcef4a8049c646f67421cbe
Chris <106016563+Xtosea@users.noreply.github.com>
2026-03-23 23:56:12 +0100

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,14 +1,4 @@
-import { io } from "socket.io-client";
-
-export const socket = io("https://afribook-backend.onrender.com", {
-  transports: ["polling", "websocket"], // keep polling first for fallback
-  withCredentials: true, // if you use cookies/auth
-});
-
-socket.on("connect", () => {
-  console.log("Connected!", socket.id); // should log the SID
-});
-
-socket.on("disconnect", () => {
-  console.log("Disconnected");
+export const socket = io("https://afribook-backend.onrender.com", { // no trailing slash
+  transports: ["polling", "websocket"],
+  auth: { token },
 });
