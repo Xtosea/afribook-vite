0d2052dffd5154f4231603901d8e0b4e4fe8c203
Chris <106016563+Xtosea@users.noreply.github.com>
2026-03-23 11:32:06 +0100

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-export const socket = io("https://africbook.globelynks.com", {
+export const socket = io("https://media.africbook.globelynks.com", {
   transports: ["websocket"],
 });
