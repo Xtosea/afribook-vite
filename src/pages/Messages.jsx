33c62f2041da6433a7c257b448e01203cb549705
Chris <106016563+Xtosea@users.noreply.github.com>
2026-03-23 17:16:40 +0100

    Create Messages.jsx

---
+++ src/pages/Messages.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import ChatSidebar from "../components/chat/ChatSidebar";
+import ChatWindow from "../components/chat/ChatWindow";
+import { socket } from "../socket";
+
+const Messages = () => {
+  const currentUserId = localStorage.getItem("userId");
+
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.emit("join", currentUserId);
+
+    socket.on("receive-message", (msg) => {
+      setMessages((prev) => [...prev, msg]);
+    });
+
+    return () => socket.off("receive-message");
+  }, []);
+
+  return (
+    <div className="h-screen flex bg-gray-100">
+
+      {/* LEFT SIDEBAR */}
+      <ChatSidebar onSelectUser={setSelectedUser} />
+
+      {/* CHAT WINDOW */}
+      <ChatWindow
+        selectedUser={selectedUser}
+        messages={messages}
+        setMessages={setMessages}
+      />
+
+    </div>
+  );
+};
+
+export default Messages;
