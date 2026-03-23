acfee310ae9cc7e1d45e2b12f89fe702b83ca2bb
Chris <106016563+Xtosea@users.noreply.github.com>
2026-03-23 21:18:10 +0100

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 // src/pages/Profile.jsx
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import PostCard from "../components/// src/pages/Profile.jsx
+import PostCard from "../components/// src/pages/Profile.jsx";
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import PostCard from "../components/PostCard";
