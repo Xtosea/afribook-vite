8adedf779db46c5850f84ef0124da903f29d1773
Chris <106016563+Xtosea@users.noreply.github.com>
2026-03-27 17:58:22 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/afribook/", // ✅ This tells Vite that your app is hosted under /afribook/
-  server: {
-    historyApiFallback: true, // for SPA routing
-  },
-});
+  base: '/afribook/', // required for Cloudflare Pages subdirectory hosting
+})
