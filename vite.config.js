23c3e604bede54a32708f75a2928ebd16dc31408
Chris <106016563+Xtosea@users.noreply.github.com>
2026-03-27 17:43:17 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/afribook/', // ✅ ensures all asset URLs have the correct prefix
+  base: "/afribook/", // ✅ This tells Vite that your app is hosted under /afribook/
+  server: {
+    historyApiFallback: true, // for SPA routing
+  },
 });
