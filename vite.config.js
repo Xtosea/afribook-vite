46b8a63dd697c94ffe005d6605441ef89b07db36
Chris <106016563+Xtosea@users.noreply.github.com>
2026-03-27 17:33:56 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/afribook/", // ✅ Add this line for subfolder deployment
+  base: "/afribook/", // ✅ important for subfolder hosting
 });
