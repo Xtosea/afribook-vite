9c671ce18b3df912552d692f5fdd867a901cb70c
Chris <106016563+Xtosea@users.noreply.github.com>
2026-03-29 00:14:55 +0100

    Update PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -137,7 +137,7 @@ const PostCard = ({
   };
 
   return (
-    <div className="bg-white p-4 rounded-xl shadow space-y-3 w-full max-w-[680px] mx-auto">
+    <div className="bg-white p-4 rounded-xl shadow space-y-3 w-full max-w-[720px] mx-auto">
 
       {/* HEADER */}
       <div className="flex items-center gap-3">
