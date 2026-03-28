230eba39e4ba5e6a7e64cd8681fde72eefc08ed4
Chris <106016563+Xtosea@users.noreply.github.com>
2026-03-28 23:56:31 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -179,7 +179,7 @@ const Home = () => {
       <div className="hidden md:block"><SidebarLeft /></div>
 
       {/* MAIN FEED */}
-      <div className="md:col-span-2 space-y-4 w-full max-w-[720px] mx-auto">
+      <div className="md:col-span-2 space-y-4 w-full">
         <StoriesBar user={currentUser} stories={stories} />
 
         {/* CREATE POST */}
