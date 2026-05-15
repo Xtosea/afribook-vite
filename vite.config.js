4b493e514a57554c80780521ac5553c7df66f2e7
Chris <106016563+Xtosea@users.noreply.github.com>
2026-05-16 00:44:56 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,61 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import { VitePWA } from "vite-plugin-pwa";
+
 export default defineConfig({
-  plugins: [react()],
-  base: "/", // ✅ change from /afribook/ to /
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      includeAssets: [
+        "favicon.svg",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+
+      manifest: {
+        name: "AfriBook",
+        short_name: "AfriBook",
+
+        description:
+          "AfriBook social media platform",
+
+        theme_color: "#000000",
+
+        background_color: "#000000",
+
+        display: "standalone",
+
+        orientation: "portrait",
+
+        scope: "/",
+
+        start_url: "/",
+
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
