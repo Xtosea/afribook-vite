8cc9c6a1dd25f5a5a87075a72d53a296f4dd8537
Chris <106016563+Xtosea@users.noreply.github.com>
2026-05-16 17:51:38 +0100

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -95,6 +95,7 @@ export default function Register() {
  </div>
     </div>
 
+);
 <div>
 <script async="async" data-cfasync="false" src="https://pl29467278.effectivecpmnetwork.com/1ac49ab91139c0ad3e13572497cfbe18/invoke.js"></script>
 <div id="container-1ac49ab91139c0ad3e13572497cfbe18"></div>
@@ -105,5 +106,5 @@ export default function Register() {
 
 
 
-  );
+  
 }
