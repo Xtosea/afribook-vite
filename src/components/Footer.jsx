67c8cbe784f7c68ee79e2f61ca81e2fe2ca31da9
Chris <106016563+Xtosea@users.noreply.github.com>
2026-05-17 13:51:12 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Foot = () => {
 return (
     <footer className="bg-gray-800 text-white py-4 mt-8">
       <div className="container mx-auto text-center">
-        <p>© {new Date().getFullYear()} Afrbook. All rights reserved.</p>
+        <p>© {new Date().getFullYear()} AfricSocial. All rights reserved.Powered by Board Givers International Multipurpose Limited</p>
         <div className="mt-2 flex justify-center space-x-4">
           <a href="/about" className="hover:underline">
             About
