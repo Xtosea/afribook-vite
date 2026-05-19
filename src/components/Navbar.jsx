8cc7ec5700a1948ac914c6ab65b4f86e9e57dcd0
Chris <106016563+Xtosea@users.noreply.github.com>
2026-05-19 17:04:24 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -193,7 +193,7 @@ const Navbar = () => {
       {/* NAVBAR */}
       {/* ========================= */}
 
-      <nav className="bg-white shadow px-4 md:px-6 py-3 flex justify-between items-center fixed top-0 left-0 right-0 z-50">
+      <nav className="bg-white shadow px-4 md:px-6 py-3 flex justify-between items-center sticky top-0 z-50">
 
         {/* LOGO */}
         <Link
