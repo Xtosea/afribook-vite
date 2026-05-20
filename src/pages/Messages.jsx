3b601331424830d4f9f4d8bfa817ddf7f69938e8
Chris <106016563+Xtosea@users.noreply.github.com>
2026-05-20 15:24:19 +0100

    Update Messages.jsx

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -316,7 +316,7 @@ const Messages = () => {
   }, []);
  
  return(
-  <div className="flex-1 flex flex-col min-h-0 overflow-hidden">
+  <div className="flex-1 flex flex-col min-h-10 overflow-hidden">
 
       {/* MOBILE OVERLAY */}
       {showSidebar && (
