79b1b98afc53bcc5f1b7c5f6f39cf8e916c51e4d
Chris <106016563+Xtosea@users.noreply.github.com>
2026-05-21 15:15:50 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
 import Navbar from "./components/Navbar";
-
 import ProtectedRoute from "./components/ProtectedRoute";
 
 import Home from "./pages/Home";
@@ -19,7 +18,6 @@ import Reels from "./pages/Reels";
 import Messages from "./pages/Messages";
 import Chat from "./pages/Chat";
 import WelcomeOnboarding from "./pages/WelcomeOnboarding";
-
 import SyncContacts from "./pages/SyncContacts";
 import EditProfile from "./pages/EditProfile";
 import MediaViewer from "./pages/MediaViewer";
@@ -32,83 +30,71 @@ import Wallet from "./pages/Wallet";
 import Leaderboard from "./pages/Leaderboard";
 import PostPage from "./pages/PostPage";
 import SavedPosts from "./pages/SavedPosts";
+
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-
-
 
 function App() {
   console.log("API:", import.meta.env.VITE_API_BASE);
 
   return (
-  <HelmetProvider>
-    <Router>
+    <HelmetProvider>
+      <Router>
+        <Navbar />
 
-      {/* Navbar */}
-      <Navbar />
+        <div className="min-h-[calc(100vh-80px)] w-full">
+          <Routes>
 
-      {/* Main Content */}
-      <div className="min-h-[calc(100vh-80px)] w-full">
+            {/* PUBLIC */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password/:token" element={<ResetPassword />} />
+            <Route path="/verify-email/:token" element={<VerifyEmail />} />
+            <Route path="/verify-success" element={<VerifySuccess />} />
+            <Route path="/verify-failed" element={<VerifyFailed />} />
+            <Route path="/welcome" element={<WelcomeOnboarding />} />
+            <Route path="/media/:postId" element={<MediaViewer />} />
+            <Route path="/post/:id" element={<PostView />} />
 
-        <Routes>
+            {/* PROTECTED */}
+            <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+            <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+            <Route path="/profile/:userId" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+            <Route path="/create" element={<ProtectedRoute><CreatePost /></ProtectedRoute>} />
+            <Route path="/reels" element={<ProtectedRoute><Reels /></ProtectedRoute>} />
+            <Route path="/messages" element={<ProtectedRoute><Messages /></ProtectedRoute>} />
+            <Route path="/chat" element={<ProtectedRoute><Chat /></ProtectedRoute>} />
+            <Route path="/wallet" element={<ProtectedRoute><Wallet /></ProtectedRoute>} />
 
-          {/* PUBLIC */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password/:token" element={<ResetPassword />} />
-          <Route path="/verify-email/:token" element={<VerifyEmail />} />
-          <Route path="/verify-success" element={<VerifySuccess />} />
-          <Route path="/verify-failed" element={<VerifyFailed />} />
-          <Route path="/welcome" element={<WelcomeOnboarding />} />
-          <Route path="/media/:postId" element={<MediaViewer />} />
-          <Route path="/post/:id" element={<PostView />} />
+            <Route path="/add-friends" element={<ProtectedRoute><AddFriends /></ProtectedRoute>} />
+            <Route path="/sync-contacts" element={<ProtectedRoute><SyncContacts /></ProtectedRoute>} />
+            <Route path="/edit-profile" element={<ProtectedRoute><EditProfile /></ProtectedRoute>} />
+            <Route path="/friend-requests" element={<ProtectedRoute><FriendRequests /></ProtectedRoute>} />
+            <Route path="/friends" element={<ProtectedRoute><FriendsList /></ProtectedRoute>} />
+            <Route path="/friend-suggestions" element={<ProtectedRoute><FriendSuggestions /></ProtectedRoute>} />
 
-          {/* PROTECTED */}
-          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-          <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-          <Route path="/profile/:userId" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-          <Route path="/create" element={<ProtectedRoute><CreatePost /></ProtectedRoute>} />
-          <Route path="/reels" element={<ProtectedRoute><Reels /></ProtectedRoute>} />
-          <Route path="/messages" element={<ProtectedRoute><Messages /></ProtectedRoute>} />
-          <Route path="/chat" element={<ProtectedRoute><Chat /></ProtectedRoute>} />
-          <Route path="/wallet" element={<ProtectedRoute><Wallet /></ProtectedRoute>} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/post/:id" element={<PostPage />} />
 
-          <Route path="/add-friends" element={<ProtectedRoute><AddFriends /></ProtectedRoute>} />
-          <Route path="/sync-contacts" element={<ProtectedRoute><SyncContacts /></ProtectedRoute>} />
-          <Route path="/edit-profile" element={<ProtectedRoute><EditProfile /></ProtectedRoute>} />
-          <Route path="/friend-requests" element={<ProtectedRoute><FriendRequests /></ProtectedRoute>} />
-          <Route path="/friends" element={<ProtectedRoute><FriendsList /></ProtectedRoute>} />
-          <Route path="/friend-suggestions" element={<ProtectedRoute><FriendSuggestions /></ProtectedRoute>} />
+            <Route
+              path="/saved"
+              element={
+                <ProtectedRoute>
+                  <SavedPosts />
+                </ProtectedRoute>
+              }
+            />
 
-          {/* NORMAL ROUTES */}
-          <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="/post/:id" element={<PostPage />} />
+          </Routes>
+        </div>
 
-          <Route
-            path="/saved"
-            element={
-              <ProtectedRoute>
-                <SavedPosts />
-              </ProtectedRoute>
-            }
-          />
+        {/* Toast MUST be inside Router but OUTSIDE Routes */}
+        <ToastContainer position="bottom-center" />
 
-          <>
-      {/* your routes */}
-      <ToastContainer />
-    </>
-
-     </Router>
-  </HelmetProvider>
-
-        </Routes>
-
-      </div> {/* ✅ THIS WAS MISSING */}
-
-   
-);
+      </Router>
+    </HelmetProvider>
+  );
 }
 
 export default App;
