b43be3efe3b131ba18f490fa5ded51a25b4f88e5
Chris <106016563+Xtosea@users.noreply.github.com>
2026-05-21 19:22:58 +0100

    Update InstallPWAButton.jsx

--- src/components/InstallPWAButton.jsx
+++ src/components/InstallPWAButton.jsx
@@ -77,7 +77,7 @@ export default function InstallPWAButton() {
   if (!showButton) return null;
 
   return (
-    <div className="fixed bottom-[1000px]  left-1/2 -translate-x-1/2 z-[9999]">
+    <div className="fixed bottom-[500px]  left-1/2 -translate-x-1/2 z-[9999]">
   <button
     onClick={handleInstall}
     className="bg-blue-600 text-white px-4 py-3 rounded-full shadow-xl"
