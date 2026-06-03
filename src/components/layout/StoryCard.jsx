3316f2b55682dbd5ddf2deaecb46f6ef4c31c949
Chris <106016563+Xtosea@users.noreply.github.com>
2026-06-03 15:35:17 +0100

    Update StoryCard.jsx

--- src/components/layout/StoryCard.jsx
+++ src/components/layout/StoryCard.jsx
@@ -54,28 +54,21 @@ const StoryCard = ({
             }
           `}
         >
-          img
-            src={
-  safeUser.profilePic
-    ? safeUser.profilePic.startsWith(
-        "http"
-      )
-      ? safeUser.profilePic
-      : `${API_BASE}${safeUser.profilePic}`
-    : "/default-avatar.png"
-}
-
-
-            alt=""
-            className="
-              w-12
-              h-12
-              rounded-full
-              object-cover
-              border-2
-              border-black
-            "
-          />
+          <img
+  src={
+    story.user?.profilePic ||
+    "/default-avatar.png"
+  }
+  alt=""
+  className="
+    w-12
+    h-12
+    rounded-full
+    object-cover
+    border-2
+    border-black
+  "
+/>
 
 
         </div>
