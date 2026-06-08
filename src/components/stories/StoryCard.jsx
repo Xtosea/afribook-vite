ea86776e63ca1afab7733050b5207b710bdd1c5a
Chris <106016563+Xtosea@users.noreply.github.com>
2026-06-08 09:36:22 +0100

    Update StoryCard.jsx

--- src/components/stories/StoryCard.jsx
+++ src/components/stories/StoryCard.jsx
@@ -5,7 +5,7 @@ const StoryCard = ({
   viewed,
   onOpen,
 }) => {
-  const media = story.media?.[0];
+  const media = story?.media?.[0];
 
   return (
     <div
@@ -23,19 +23,64 @@ const StoryCard = ({
       "
     >
       {/* MEDIA */}
-      {media?.type === "image" ? (
+      {media?.type === "image" && (
         <img
           src={media.url}
           alt=""
           className="w-full h-full object-cover"
         />
-      ) : (
+      )}
+
+      {media?.type === "video" && (
         <video
-          src={media?.url}
+          src={media.url}
           className="w-full h-full object-cover"
           muted
           playsInline
+          preload="metadata"
         />
+      )}
+
+      {media?.type === "audio" && (
+        <div
+          className="
+            w-full
+            h-full
+            flex
+            items-center
+            justify-center
+            bg-gradient-to-br
+            from-purple-900
+            via-black
+            to-blue-900
+          "
+        >
+          <div className="text-center text-white">
+            <div className="text-5xl">
+              🎵
+            </div>
+
+            <p className="text-xs mt-2">
+              Audio Story
+            </p>
+          </div>
+        </div>
+      )}
+
+      {!media && (
+        <div
+          className="
+            w-full
+            h-full
+            flex
+            items-center
+            justify-center
+            bg-gray-900
+            text-white
+          "
+        >
+          No Media
+        </div>
       )}
 
       {/* OVERLAY */}
@@ -54,42 +99,42 @@ const StoryCard = ({
             }
           `}
         >
-  <img
-  src={
-    story.user?.profilePic ||
-    "/default-avatar.png"
-  }
-  alt=""
-  className="
-    w-12
-    h-12
-    rounded-full
-    object-cover
-    border-2
-    border-black
-  "
-/>
+          <img
+            src={
+              story?.user?.profilePic ||
+              "/default-avatar.png"
+            }
+            alt=""
+            className="
+              w-12
+              h-12
+              rounded-full
+              object-cover
+              border-2
+              border-black
+            "
+          />
+        </div>
+      </div>
 
-</div>
- </div>
+      {/* MUSIC ICON */}
+      {story?.music && (
+        <div className="absolute top-2 right-2 text-lg">
+          🎵
+        </div>
+      )}
 
-      {story.music && (
-  <div className="absolute top-2 right-2">
-    🎵
-  </div>
-)}
-
-
-{story.text && (
-  <div className="absolute bottom-10 left-2">
-    ✍️
-  </div>
-)}
+      {/* TEXT ICON */}
+      {story?.text && (
+        <div className="absolute bottom-10 left-2 text-lg">
+          ✍️
+        </div>
+      )}
 
       {/* USERNAME */}
       <div className="absolute bottom-3 left-3 right-3">
         <p className="text-white text-sm font-semibold truncate">
-          {story.user?.name || "Unknown"}
+          {story?.user?.name || "Unknown"}
         </p>
       </div>
     </div>
