57be6da3de6e21d006f3758a5627234800ab8401
Chris <106016563+Xtosea@users.noreply.github.com>
2026-06-13 12:54:37 +0100

    Create validateVideoDuration.js

---
+++ src/utils/validateVideoDuration.js
@@ -0,0 +1,30 @@
+const validateVideoDuration = (
+  file,
+  maxSeconds = 60
+) => {
+  return new Promise((resolve, reject) => {
+    const video = document.createElement("video");
+
+    video.preload = "metadata";
+
+    video.onloadedmetadata = () => {
+      URL.revokeObjectURL(video.src);
+
+      if (video.duration > maxSeconds) {
+        reject(
+          `Video exceeds ${maxSeconds} seconds`
+        );
+      } else {
+        resolve(video.duration);
+      }
+    };
+
+    video.onerror = () => {
+      reject("Invalid video file");
+    };
+
+    video.src = URL.createObjectURL(file);
+  });
+};
+
+export default validateVideoDuration;
