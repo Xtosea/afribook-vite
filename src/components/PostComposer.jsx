3c23ef15727e1d5933d16ff722b519d15a5d2314
Chris <106016563+Xtosea@users.noreply.github.com>
2026-06-15 08:04:06 +0100

    Update PostComposer.jsx

--- src/components/PostComposer.jsx
+++ src/components/PostComposer.jsx
@@ -32,7 +32,7 @@ import("emoji-picker-react")
 );
 
 
-const [showTextArea, setShowTextArea] = useState(false);
+const [showTextArea, setShowTextArea] = useState(true);
 const [posting, setPosting] =
 useState(false);
 
