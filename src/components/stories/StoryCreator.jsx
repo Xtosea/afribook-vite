65a09984a7ac59ad12736a8274bc6386dd4dfc43
Chris <106016563+Xtosea@users.noreply.github.com>
2026-06-17 02:48:51 +0100

    Update StoryCreator.jsx

--- src/components/stories/StoryCreator.jsx
+++ src/components/stories/StoryCreator.jsx
@@ -8,7 +8,7 @@ import { API_BASE } from "../../api/api";
 import Draggable from "react-draggable";
 import {
   useAIEffects,
-} from "../../hooks/useAIEffects";
+} from "../../hooks/useAIEnhance";
 
 
 const emojiList = [
