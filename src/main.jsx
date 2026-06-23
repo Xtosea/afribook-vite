fbda8522dc5b221833e15f0f077a9eb25243239b
Chris <106016563+Xtosea@users.noreply.github.com>
2026-06-23 20:57:46 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,20 @@ import { AuthProvider } from "./context/AuthContext";
 
 import { registerSW } from "virtual:pwa-register";
 
+import Logout from "./components/inactivity/Logout";
+
 registerSW({
   immediate: true,
 });
+
+
 
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
   <React.StrictMode>
     <AuthProvider>
+      <Logout />
       <App />
     </AuthProvider>
   </React.StrictMode>
