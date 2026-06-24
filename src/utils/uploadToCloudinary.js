6d3065f4ec1fa04ff201d0f07cd0ffbc32cdd970
Chris <106016563+Xtosea@users.noreply.github.com>
2026-06-24 23:04:55 +0100

    Update uploadToCloudinary.js

--- src/utils/uploadToCloudinary.js
+++ src/utils/uploadToCloudinary.js
@@ -1,33 +1,17 @@
 export async function uploadToCloudinary(file) {
 
-  alert(
-    "URL = " +
-    import.meta.env.VITE_CLOUDINARY_UPLOAD_URL
-  );
-
-  alert(
-    "PRESET = " +
-    import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET
-  );
-
   const formData = new FormData();
 
-  formData.append("file", file);
+  formData.append(
+    "file",
+    file
+  );
 
   formData.append(
     "upload_preset",
     import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET
   );
 
-  console.log(
-    "CLOUDINARY URL:",
-    import.meta.env.VITE_CLOUDINARY_UPLOAD_URL
-  );
-
-  alert(
-    "Uploading to:\n" +
-    import.meta.env.VITE_CLOUDINARY_UPLOAD_URL
-  );
 
   const response = await fetch(
     import.meta.env.VITE_CLOUDINARY_UPLOAD_URL,
@@ -37,16 +21,22 @@ export async function uploadToCloudinary(file) {
     }
   );
 
+
   const data = await response.json();
 
+
   if (!response.ok) {
-    console.error("Cloudinary error:", data);
+    console.error(
+      "Cloudinary error:",
+      data
+    );
 
     throw new Error(
       data.error?.message ||
       "Cloudinary upload failed"
     );
   }
+
 
   return data.secure_url;
 }
