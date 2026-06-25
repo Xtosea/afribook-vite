c2ff66f30b5b5e25a63092b7997cadf97969c412
Chris <106016563+Xtosea@users.noreply.github.com>
2026-06-25 13:22:00 +0100

    Update DateDivider.jsx

--- src/components/DateDivider.jsx
+++ src/components/DateDivider.jsx
@@ -1,8 +1,19 @@
 export default function DateDivider({ date }) {
   return (
     <div className="flex justify-center my-4">
-      <span className="bg-red-500 text-white px-4 py-2 rounded-full">
-        TEST DATE: {date}
+      <span
+        className="
+          bg-gray-300
+          text-gray-800
+          text-xs
+          font-medium
+          px-3
+          py-1
+          rounded-full
+          shadow
+        "
+      >
+        {date}
       </span>
     </div>
   );
