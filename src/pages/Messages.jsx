efdda8a0c276af4c4f116d942ea3c94f47d52d59
Chris <106016563+Xtosea@users.noreply.github.com>
2026-06-26 10:30:10 +0100

    Update Messages.jsx

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -858,7 +858,7 @@ console.log(
 
 
             {/* MESSAGES */}
-            <div className="flex-1 overflow-y-auto px-3 pt-20 pb-32 space-y-4">
+            <div className="flex-1 overflow-y-auto px-3 pt-24 pb-36 space-y-4">
 
               {Object.entries(groupedMessages).map(
   ([date, dayMessages]) => (
