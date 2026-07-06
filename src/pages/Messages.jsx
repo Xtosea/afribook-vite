12cb0cac958ec740751d73404294858ffa0d0960
Chris <106016563+Xtosea@users.noreply.github.com>
2026-07-06 01:36:04 +0100

    Update Messages.jsx

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -476,17 +476,12 @@ socket.on(
     }
   };
 
-  const webRTC = useWebRTC({
-  currentUser,
-  selectedUser,
-  socket: socketRef.current,
-  video: false,
-});
+  
 
 // 👇 Add this here
 useEffect(() => {
   if (webRTC.receivingCall) {
-    if (webRTC.incomingVideo) {
+    if (webRTC.incomingVideo) 
       setShowCall(true);
     } else {
       setShowVoiceCall(true);
