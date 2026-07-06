8b1e56aced367908b25455a1c31beda3847e3e2a
Chris <106016563+Xtosea@users.noreply.github.com>
2026-07-07 00:49:27 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ import { AuthProvider } from "./context/AuthContext";
 import { registerSW } from "virtual:pwa-register";
 
 import Logout from "./components/inactivity/Logout";
-import CallProvider from "../features/calls/CallProvider";
+import CallProvider from "./context/CallProvider";
 
 
 window.global = window;
