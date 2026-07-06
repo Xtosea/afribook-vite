29bc500027589bd593e87cf44f219d6bedefc5ec
Chris <106016563+Xtosea@users.noreply.github.com>
2026-07-06 17:13:26 +0100

    Create IncomingCallListener.jsx

---
+++ src/features/calls/IncomingCallListener.jsx
@@ -0,0 +1,58 @@
+import { useEffect } from "react";
+
+import {
+  connectSocket,
+} from "../../socket";
+
+import {
+  useCall,
+} from "../../context/CallContext";
+
+const IncomingCallListener = () => {
+
+  const {
+    setIncomingCall,
+    setShowIncomingCall,
+  } = useCall();
+
+  useEffect(() => {
+
+    const socket = connectSocket();
+
+    if (!socket) return;
+
+    const handleIncomingCall = (data) => {
+
+      console.log(
+        "📞 Global incoming call:",
+        data
+      );
+
+      setIncomingCall(data);
+
+      setShowIncomingCall(true);
+    };
+
+    socket.on(
+      "incoming-call",
+      handleIncomingCall
+    );
+
+    return () => {
+
+      socket.off(
+        "incoming-call",
+        handleIncomingCall
+      );
+
+    };
+
+  }, [
+    setIncomingCall,
+    setShowIncomingCall,
+  ]);
+
+  return null;
+};
+
+export default IncomingCallListener;
