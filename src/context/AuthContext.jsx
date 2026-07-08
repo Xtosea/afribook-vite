78d24b3dc4da5164201f44d53bfbe38355b8671c
Chris <106016563+Xtosea@users.noreply.github.com>
2026-07-08 04:15:02 +0100

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,17 +1,54 @@
-import { createContext, useContext, useState } from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+} from "react";
 
 export const AuthContext = createContext();
 
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-  const [token] = useState(localStorage.getItem("token"));
-  const [currentUser] = useState(
-    JSON.parse(localStorage.getItem("user"))
+  const [token, setToken] = useState(
+    localStorage.getItem("token")
   );
 
+  const [currentUser, setCurrentUser] =
+    useState(
+      JSON.parse(
+        localStorage.getItem("user") || "null"
+      )
+    );
+
+  const login = (token, user) => {
+    localStorage.setItem("token", token);
+    localStorage.setItem(
+      "user",
+      JSON.stringify(user)
+    );
+    localStorage.setItem("userId", user._id);
+
+    setToken(token);
+    setCurrentUser(user);
+  };
+
+  const logout = () => {
+    localStorage.clear();
+
+    setToken(null);
+    setCurrentUser(null);
+  };
+
   return (
-    <AuthContext.Provider value={{ token, currentUser }}>
+    <AuthContext.Provider
+      value={{
+        token,
+        currentUser,
+        isLoggedIn: !!token,
+        login,
+        logout,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
