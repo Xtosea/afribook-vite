f0363ed85d18eb340d31d9be06bba8fa57afdf30
Chris <106016563+Xtosea@users.noreply.github.com>
2026-07-08 07:40:53 +0100

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { API_BASE } from "../api/api";
-import { useAuth } from "../context/AuthContext";.
+import { useAuth } from "../context/AuthContext";
 
 
 export default function Login() {
