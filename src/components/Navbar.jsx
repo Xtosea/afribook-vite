491e789a55f6beeec42918822ff2502772c930fe
Chris <106016563+Xtosea@users.noreply.github.com>
2026-07-08 02:05:46 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -359,7 +359,7 @@ className="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center"
 
 </button>
 
-</div>
+
 
 {/* 👥 SOCIAL */}
 
@@ -560,7 +560,7 @@ Logout
 </div>
 
 </div>
-</div>
+
 
 )}
           
