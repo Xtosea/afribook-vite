3f20a368d1aa59b7d4916a82bdcf645b70d31865
Chris <106016563+Xtosea@users.noreply.github.com>
2026-07-10 17:35:12 +0100

    Create data/africa/Countries.jsx

---
+++ src/data/Africa/data/africa/Countries.jsx
@@ -0,0 +1,94 @@
+const countries = [
+  {
+    code: "NG",
+    name: "Nigeria",
+    currency: "NGN",
+    phoneCode: "+234",
+  },
+  {
+    code: "GH",
+    name: "Ghana",
+    currency: "GHS",
+    phoneCode: "+233",
+  },
+  {
+    code: "KE",
+    name: "Kenya",
+    currency: "KES",
+    phoneCode: "+254",
+  },
+  {
+    code: "ZA",
+    name: "South Africa",
+    currency: "ZAR",
+    phoneCode: "+27",
+  },
+  {
+    code: "UG",
+    name: "Uganda",
+    currency: "UGX",
+    phoneCode: "+256",
+  },
+  {
+    code: "TZ",
+    name: "Tanzania",
+    currency: "TZS",
+    phoneCode: "+255",
+  },
+  {
+    code: "RW",
+    name: "Rwanda",
+    currency: "RWF",
+    phoneCode: "+250",
+  },
+  {
+    code: "CM",
+    name: "Cameroon",
+    currency: "XAF",
+    phoneCode: "+237",
+  },
+  {
+    code: "SN",
+    name: "Senegal",
+    currency: "XOF",
+    phoneCode: "+221",
+  },
+  {
+    code: "CI",
+    name: "Ivory Coast",
+    currency: "XOF",
+    phoneCode: "+225",
+  },
+  {
+    code: "ZM",
+    name: "Zambia",
+    currency: "ZMW",
+    phoneCode: "+260",
+  },
+  {
+    code: "ZW",
+    name: "Zimbabwe",
+    currency: "USD",
+    phoneCode: "+263",
+  },
+  {
+    code: "BW",
+    name: "Botswana",
+    currency: "BWP",
+    phoneCode: "+267",
+  },
+  {
+    code: "NA",
+    name: "Namibia",
+    currency: "NAD",
+    phoneCode: "+264",
+  },
+  {
+    code: "ET",
+    name: "Ethiopia",
+    currency: "ETB",
+    phoneCode: "+251",
+  },
+];
+
+export default countries;
