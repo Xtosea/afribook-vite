8a06627af44e2372253b3e1c611960d0dc90999e
Chris <106016563+Xtosea@users.noreply.github.com>
2026-07-10 17:55:56 +0100

    Create southAfrica.js

---
+++ src/data/africa/southAfrica.js
@@ -0,0 +1,95 @@
+const southAfrica = {
+  Gauteng: [
+    "Johannesburg",
+    "Pretoria",
+    "Soweto",
+    "Midrand",
+    "Sandton",
+    "Centurion",
+    "Kempton Park",
+    "Benoni",
+    "Boksburg",
+    "Alberton",
+    "Roodepoort",
+    "Randburg",
+  ],
+
+  "Western Cape": [
+    "Cape Town",
+    "Stellenbosch",
+    "Paarl",
+    "George",
+    "Mossel Bay",
+    "Hermanus",
+    "Worcester",
+    "Somerset West",
+  ],
+
+  "KwaZulu-Natal": [
+    "Durban",
+    "Pietermaritzburg",
+    "Richards Bay",
+    "Newcastle",
+    "Ladysmith",
+    "Empangeni",
+    "Ballito",
+    "Port Shepstone",
+  ],
+
+  "Eastern Cape": [
+    "Gqeberha",
+    "East London",
+    "Mthatha",
+    "Queenstown",
+    "Grahamstown",
+    "Jeffreys Bay",
+    "Uitenhage",
+  ],
+
+  "Free State": [
+    "Bloemfontein",
+    "Welkom",
+    "Bethlehem",
+    "Kroonstad",
+    "Sasolburg",
+    "Virginia",
+  ],
+
+  Limpopo: [
+    "Polokwane",
+    "Tzaneen",
+    "Mokopane",
+    "Louis Trichardt",
+    "Musina",
+    "Thohoyandou",
+  ],
+
+  Mpumalanga: [
+    "Mbombela",
+    "Witbank",
+    "Secunda",
+    "Middelburg",
+    "Ermelo",
+    "Standerton",
+  ],
+
+  "North West": [
+    "Mahikeng",
+    "Rustenburg",
+    "Klerksdorp",
+    "Potchefstroom",
+    "Brits",
+    "Lichtenburg",
+  ],
+
+  "Northern Cape": [
+    "Kimberley",
+    "Upington",
+    "Kuruman",
+    "Springbok",
+    "De Aar",
+    "Colesberg",
+  ],
+};
+
+export default southAfrica;
