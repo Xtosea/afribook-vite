a9d1ee020217bdc35b639a291d2fd560fc470736
Chris <106016563+Xtosea@users.noreply.github.com>
2026-07-10 17:51:59 +0100

    Create ghana.js

---
+++ src/data/africa/ghana.js
@@ -0,0 +1,151 @@
+const ghana = {
+  "Greater Accra": [
+    "Accra",
+    "Tema",
+    "Madina",
+    "Kasoa",
+    "Adenta",
+    "Teshie",
+    "Nungua",
+    "Amasaman",
+    "Ashaiman",
+    "Dansoman",
+  ],
+
+  Ashanti: [
+    "Kumasi",
+    "Obuasi",
+    "Ejisu",
+    "Mampong",
+    "Konongo",
+    "Bekwai",
+    "Offinso",
+    "Asokwa",
+    "Tafo",
+    "Effiduase",
+  ],
+
+  Central: [
+    "Cape Coast",
+    "Winneba",
+    "Kasoa",
+    "Elmina",
+    "Mankessim",
+    "Saltpond",
+    "Swedru",
+    "Dunkwa-on-Offin",
+  ],
+
+  Eastern: [
+    "Koforidua",
+    "Nkawkaw",
+    "Akosombo",
+    "Akim Oda",
+    "Nsawam",
+    "Suhum",
+    "Aburi",
+    "Begoro",
+  ],
+
+  Western: [
+    "Sekondi",
+    "Takoradi",
+    "Axim",
+    "Tarkwa",
+    "Prestea",
+    "Shama",
+    "Mpohor",
+  ],
+
+  "Western North": [
+    "Sefwi Wiawso",
+    "Bibiani",
+    "Juaboso",
+    "Bodi",
+    "Aowin",
+  ],
+
+  Volta: [
+    "Ho",
+    "Hohoe",
+    "Keta",
+    "Anloga",
+    "Aflao",
+    "Dzodze",
+    "Kpando",
+  ],
+
+  Oti: [
+    "Dambai",
+    "Jasikan",
+    "Nkwanta",
+    "Kadjebi",
+    "Kete Krachi",
+  ],
+
+  Northern: [
+    "Tamale",
+    "Savelugu",
+    "Yendi",
+    "Walewale",
+    "Karaga",
+    "Kpandai",
+  ],
+
+  Savannah: [
+    "Damongo",
+    "Bole",
+    "Sawla",
+    "Salaga",
+    "Larabanga",
+  ],
+
+  "North East": [
+    "Nalerigu",
+    "Walewale",
+    "Gambaga",
+    "Bunkpurugu",
+  ],
+
+  Upper East: [
+    "Bolgatanga",
+    "Navrongo",
+    "Bawku",
+    "Zebilla",
+    "Paga",
+  ],
+
+  Upper West: [
+    "Wa",
+    "Lawra",
+    "Tumu",
+    "Nadowli",
+    "Jirapa",
+  ],
+
+  Bono: [
+    "Sunyani",
+    "Berekum",
+    "Dormaa Ahenkro",
+    "Wenchi",
+    "Fiapre",
+  ],
+
+  Bono East: [
+    "Techiman",
+    "Kintampo",
+    "Atebubu",
+    "Nkoranza",
+    "Prang",
+  ],
+
+  Ahafo: [
+    "Goaso",
+    "Bechem",
+    "Duayaw Nkwanta",
+    "Kenyasi",
+    "Hwidiem",
+  ],
+};
+
+export default ghana;
