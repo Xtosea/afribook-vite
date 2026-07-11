5e89e8c6fbe2ce83e70782acbae5b27e181e7372
Chris <106016563+Xtosea@users.noreply.github.com>
2026-07-11 11:09:09 +0100

    Update MarketplaceGrid.jsx

--- src/components/marketplace/MarketplaceGrid.jsx
+++ src/components/marketplace/MarketplaceGrid.jsx
@@ -90,8 +90,7 @@ const MarketplaceGrid = ({
   // Marketplace Cards
   // ===============================
 
-console.log("Listing:", listing);
-console.log("Listing ID:", listing._id);
+
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
