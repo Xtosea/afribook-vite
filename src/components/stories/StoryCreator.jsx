e45ded4637eeb3acd1d1beceb9099ba32d2baba6
Chris <106016563+Xtosea@users.noreply.github.com>
2026-07-30 08:06:24 +0100

    Update StoryCreator.jsx

--- src/components/stories/StoryCreator.jsx
+++ src/components/stories/StoryCreator.jsx
@@ -11,7 +11,7 @@ from "../../utils/uploadToCloudinary";
 
 import { uploadToR2 } from "../../utils/uploadToR2";
 import { generateVideoThumbnail } from "../../utils/generateVideoThumbnail";
-import StickerPicker from "../components/stickers/StickerPicker";
+import StickerPicker from "./components/stickers/StickerPicker";
 import StoryStickerLayer from "./components/stickers/StoryStickerLayer";
 import useStoryStickers from "./components/stickers/useStoryStickers";
 import StickerToolbar from "./components/stickers/StickerToolbar";
