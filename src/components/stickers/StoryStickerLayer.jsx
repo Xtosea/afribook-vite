2d55ce3df98e550905430e1fe39ac69ac1532609
Chris <106016563+Xtosea@users.noreply.github.com>
2026-07-30 07:13:33 +0100

    Create StoryStickerLayer.jsx

---
+++ src/components/stickers/StoryStickerLayer.jsx
@@ -0,0 +1,20 @@
+import StorySticker from "./StorySticker";
+
+export default function StoryStickerLayer({
+  stickers,
+  updateSticker,
+  removeSticker,
+}) {
+  return (
+    <>
+      {stickers.map((sticker) => (
+        <StorySticker
+          key={sticker.id}
+          sticker={sticker}
+          onUpdate={updateSticker}
+          onRemove={removeSticker}
+        />
+      ))}
+    </>
+  );
+}
