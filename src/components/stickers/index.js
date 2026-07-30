6d4265a40e9e161b1fccc5dd48f9d133a6c96f4b
Chris <106016563+Xtosea@users.noreply.github.com>
2026-07-30 06:04:07 +0100

    Create index.js

---
+++ src/components/stickers/index.js
@@ -0,0 +1,6 @@
+export { default as StickerPicker } from "./StickerPicker";
+export { default as StickerGrid } from "./StickerGrid";
+export { default as StickerTabs } from "./StickerTabs";
+export { default as StickerSearch } from "./StickerSearch";
+export { default as StickerRecent } from "./StickerRecent";
+export { default as StickerFavorites } from "./StickerFavorites";
