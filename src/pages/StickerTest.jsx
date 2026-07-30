20a8e6050db5b26baee28cf96ba4317169d49ce4
Chris <106016563+Xtosea@users.noreply.github.com>
2026-07-30 01:33:08 +0100

    Create StickerTest.jsx

---
+++ src/pages/StickerTest.jsx
@@ -0,0 +1,19 @@
+import { StickerPicker } from "../components/stickers";
+
+export default function StickerTest() {
+
+    function handleSticker(path){
+        console.log(path);
+        alert(path);
+    }
+
+    return (
+        <div style={{padding:20}}>
+            <h2>Sticker Test</h2>
+
+            <StickerPicker
+                onSelect={handleSticker}
+            />
+        </div>
+    );
+}
